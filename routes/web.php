54f1d32287822b9768656a735ba0daf09657a8ee
GuilhermeSalles <guibaltazarvs@gmail.com>
2024-05-04 16:46:16 -0300

    Vai adicionar rota de logout

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ Route::delete('/services/{id}', [ServiceController::class, 'destroy'])->name('se
 // Login
 Route::view('/login','login.form')->name('login.form');
 Route::post('/auth', [LoginController::class, 'auth']) ->name('login.auth');
-
+Route::get('/logout', [LoginController::class, 'logout'])->name('login.logout');
 
 Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
 
